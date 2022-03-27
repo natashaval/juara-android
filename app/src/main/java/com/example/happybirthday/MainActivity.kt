7d15ff38fbb90c2a0c468b9a5907da6ff00a0ff3
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-03-27 23:17:52 +0700

    refactor main page

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.happybirthday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val birthdayButton = findViewById<Button>(R.id.bt_birthday)
+    birthdayButton.setOnClickListener {
+      startActivity(Intent(this, BirthdayActivity::class.java))
+    }
+
   }
 }
