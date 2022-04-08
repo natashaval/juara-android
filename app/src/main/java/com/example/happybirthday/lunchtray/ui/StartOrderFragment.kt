d812d0c693ed7061379c3b534b58c129854ce222
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-04-08 20:33:35 +0700

    init start order fragment

--- app/src/main/java/com/example/happybirthday/lunchtray/ui/StartOrderFragment.kt
+++ app/src/main/java/com/example/happybirthday/lunchtray/ui/StartOrderFragment.kt
@@ -6,55 +6,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.happybirthday.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.happybirthday.databinding.FragmentStartOrderBinding
 
 /**
- * A simple [Fragment] subclass.
- * Use the [StartOrderFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * [StartOrderFragment] allows people to click the start button to start an order.
  */
 class StartOrderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // Binding object instance corresponding to the fragment_start_order.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var _binding: FragmentStartOrderBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start_order, container, false)
+        _binding = FragmentStartOrderBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        // Navigate to entree menu
+        binding.startOrderBtn.setOnClickListener {
+            // TODO: navigate to the EntreeMenuFragment
+        }
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StartOrderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StartOrderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
