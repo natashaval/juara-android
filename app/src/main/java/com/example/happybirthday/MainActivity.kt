6e4a7292149035a5e0f54a43bf07d0f2e03c5e0f
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-04-10 22:29:46 +0700

    binding adapter on main activity buttons

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.happybirthday
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
 import com.example.happybirthday.affirmation.view.AffirmationActivity
 import com.example.happybirthday.amphibians.ui.AmphibianActivity
 import com.example.happybirthday.busschedule.ui.BusActivity
@@ -16,69 +20,45 @@ import com.example.happybirthday.unscramble.ui.UnscrambleActivity
 import com.example.happybirthday.words.view.WordActivity
 
 class MainActivity : AppCompatActivity() {
-  private lateinit var binding: ActivityMainBinding
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
 
-    val birthdayButton = findViewById<Button>(R.id.bt_birthday)
-    birthdayButton.setOnClickListener {
-      startActivity(Intent(this, BirthdayActivity::class.java))
-    }
-    
-    val diceButton = findViewById<Button>(R.id.bt_dice)
-    diceButton.setOnClickListener {
-      startActivity(Intent(this, DiceActivity::class.java))
-    }
+    private lateinit var binding: ActivityMainBinding
 
-    val lemonadeButton = findViewById<Button>(R.id.bt_lemonade)
-    lemonadeButton.setOnClickListener {
-      startActivity(Intent(this, LemonadeActivity::class.java))
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-    binding.btTip.setOnClickListener {
-      startActivity(Intent(this, TipActivity::class.java))
-    }
+        val birthdayButton = findViewById<Button>(R.id.bt_birthday)
+        birthdayButton.setOnClickListener {
+            startActivity(Intent(this, BirthdayActivity::class.java))
+        }
 
-    binding.btAffirmations.setOnClickListener {
-      startActivity(Intent(this, AffirmationActivity::class.java))
+        binding.btDice.setOnClickListener {
+            startActivity(Intent(this, DiceActivity::class.java))
+        }
     }
+}
 
-    binding.btDogglers.setOnClickListener {
-      startActivity(Intent(this, DogglersActivity::class.java))
-    }
-
-    binding.btWords.setOnClickListener {
-      startActivity(Intent(this, WordActivity::class.java))
-    }
-
-    binding.btDessert.setOnClickListener {
-      startActivity(Intent(this, DessertActivity::class.java))
+@BindingAdapter("openActivity")
+fun bindActivity(button: Button, appName: String) {
+    val context = button.context
+    val resources = button.resources
+    val clazz = when (appName) {
+        resources.getString(R.string.lemonade_app_name) -> LemonadeActivity::class.java
+        resources.getString(R.string.tip_app_name) -> TipActivity::class.java
+        resources.getString(R.string.affirmation_app_name) -> AffirmationActivity::class.java
+        resources.getString(R.string.doggler_app_name) -> DogglersActivity::class.java
+        resources.getString(R.string.words_app_name) -> WordActivity::class.java
+        resources.getString(R.string.dessert_app_name) -> DessertActivity::class.java
+        resources.getString(R.string.unscramble_app_name) -> UnscrambleActivity::class.java
+        resources.getString(R.string.cupcake_app_name) -> CupcakeActivity::class.java
+        resources.getString(R.string.lunch_app_name) -> LunchActivity::class.java
+        resources.getString(R.string.mars_app_name) -> MarsActivity::class.java
+        resources.getString(R.string.amphibians_app_name) -> AmphibianActivity::class.java
+        resources.getString(R.string.bus_app_name) -> BusActivity::class.java
+        else -> MainActivity::class.java
     }
 
-    binding.btUnscramble.setOnClickListener {
-      startActivity(Intent(this, UnscrambleActivity::class.java))
+    button.setOnClickListener {
+        startActivity(context, Intent(context, clazz), null)
     }
-
-    binding.btCupcake.setOnClickListener {
-      startActivity(Intent(this, CupcakeActivity::class.java))
-    }
-
-    binding.btLunch.setOnClickListener {
-      startActivity(Intent(this, LunchActivity::class.java))
-    }
-
-    binding.btMars.setOnClickListener {
-      startActivity(Intent(this, MarsActivity::class.java))
-    }
-
-    binding.btAmphibians.setOnClickListener {
-      startActivity(Intent(this, AmphibianActivity::class.java))
-    }
-
-    binding.btBus.setOnClickListener {
-      startActivity(Intent(this, BusActivity::class.java))
-    }
-  }
 }
