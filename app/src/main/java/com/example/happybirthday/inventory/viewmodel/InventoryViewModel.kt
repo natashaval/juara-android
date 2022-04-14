ab7730bcc6a5fdd67f0179f7b3de0fd441c78081
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-04-14 23:36:51 +0700

    sell, update, and delete item

--- app/src/main/java/com/example/happybirthday/inventory/viewmodel/InventoryViewModel.kt
+++ app/src/main/java/com/example/happybirthday/inventory/viewmodel/InventoryViewModel.kt
@@ -40,6 +40,45 @@ class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
     fun retrieveItem(id: Int): LiveData<Item> {
         return itemDao.getItem(id).asLiveData()
     }
+
+    private fun updateItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.update(item)
+        }
+    }
+
+    fun sellItem(item: Item) {
+        if (item.quantityInStock > 0) {
+            val newItem = item.copy(quantityInStock = item.quantityInStock - 1)
+            updateItem(newItem)
+            // Nat: .copy() is used because quantityInStock is val,
+            // but it is also possible to directly update the item if quantityInStock is var
+        }
+    }
+
+    fun isStockAvailable(item: Item): Boolean {
+        return item.quantityInStock > 0
+    }
+
+    fun deleteItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.delete(item)
+        }
+    }
+
+    private fun getUpdatedItemEntry(itemId: Int, itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            id = itemId,
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    fun updateItem(itemId: Int, itemName: String, itemPrice: String, itemCount: String) {
+        val updatedItem = getUpdatedItemEntry(itemId, itemName, itemPrice, itemCount)
+        updateItem(updatedItem)
+    }
 }
 
 class InventoryViewModelFactory(private val itemDao: ItemDao): ViewModelProvider.Factory {
