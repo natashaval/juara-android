176c5254926e3f114e6318e76978bc9b517499e7
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-04-16 14:27:55 +0700

    cancel work and constraints

--- app/src/main/java/com/example/happybirthday/background/ui/BlurActivity.kt
+++ app/src/main/java/com/example/happybirthday/background/ui/BlurActivity.kt
@@ -1,10 +1,14 @@
 package com.example.happybirthday.background.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.work.WorkInfo
 import com.example.happybirthday.R
+import com.example.happybirthday.background.KEY_IMAGE_URI
 import com.example.happybirthday.background.viewmodel.BlurViewModel
 import com.example.happybirthday.databinding.ActivityBlurBinding
 
@@ -23,6 +27,21 @@ class BlurActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.goButton.setOnClickListener { viewModel.applyBlur(blurLevel) }
+
+        // Observe work status
+        viewModel.outputWorkInfos.observe(this, workInfosObserver())
+
+        // Setup view output image file button
+        binding.seeFileButton.setOnClickListener {
+            viewModel.outputUri?.let { currentUri ->
+                val actionView = Intent(Intent.ACTION_VIEW, currentUri)
+                actionView.resolveActivity(packageManager)?.run {
+                    startActivity(actionView)
+                }
+            }
+        }
+
+        binding.cancelButton.setOnClickListener { viewModel.cancelWork() }
     }
 
     /**
@@ -56,4 +75,38 @@ class BlurActivity : AppCompatActivity() {
                 R.id.radio_blur_lv_3 -> 3
                 else -> 1
             }
+
+    // Define the observer function
+    private fun workInfosObserver(): Observer<List<WorkInfo>> {
+        return Observer { listOfWorkInfo ->
+            // Note that these next few lines grab a single WorkInfo if it exists
+            // This code could be in a Transformation in the ViewModel; they are included here
+            // so that the entire process of displaying a WorkInfo is in one location.
+
+            // If there are no matching work info, do nothing
+            if (listOfWorkInfo.isNullOrEmpty()) {
+                return@Observer
+            }
+
+            // We only care about the one output status.
+            // Every continuation has only one worker tagged TAG_OUTPUT
+            val workInfo = listOfWorkInfo[0]
+
+            if (workInfo.state.isFinished) {
+                showWorkFinished()
+
+                // Normally this processing, which is not directly related to drawing views on
+                // screen would be in the ViewModel. For simplicity we are keeping it here.
+                val outputImageUri = workInfo.outputData.getString(KEY_IMAGE_URI)
+
+                // If there is an output file show "See File" button
+                if (!outputImageUri.isNullOrEmpty()) {
+                    viewModel.setOutputUri(outputImageUri)
+                    binding.seeFileButton.visibility = View.VISIBLE
+                } else {
+                    showWorkInProgress()
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/happybirthday/background/viewmodel/BlurViewModel.kt
+++ app/src/main/java/com/example/happybirthday/background/viewmodel/BlurViewModel.kt
@@ -4,12 +4,14 @@ import android.app.Application
 import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.work.*
 import com.example.happybirthday.R
 import com.example.happybirthday.background.IMAGE_MANIPULATION_WORK_NAME
 import com.example.happybirthday.background.KEY_IMAGE_URI
+import com.example.happybirthday.background.TAG_OUTPUT
 import com.example.happybirthday.background.workers.BlurWorker
 import com.example.happybirthday.background.workers.CleanupWorker
 import com.example.happybirthday.background.workers.SaveImageToFileWorker
@@ -20,17 +22,20 @@ class BlurViewModel(application: Application) : ViewModel() {
     internal var imageUri: Uri? = null
     internal var outputUri: Uri? = null
 
+    internal val outputWorkInfos: LiveData<List<WorkInfo>>
+
     private val workManager = WorkManager.getInstance(application)
 
     init {
         imageUri = getImageUri(application.applicationContext)
+        outputWorkInfos = workManager.getWorkInfosByTagLiveData(TAG_OUTPUT)
     }
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
     internal fun applyBlur(blurLevel: Int) {
-        // Add WorkkRequest to cleanup temporary images
+        // Add WorkRequest to cleanup temporary images
         var continuation = workManager
             .beginUniqueWork(
                 IMAGE_MANIPULATION_WORK_NAME,
@@ -52,8 +57,17 @@ class BlurViewModel(application: Application) : ViewModel() {
             continuation = continuation.then(blurBuilder.build())
         }
 
+        // Put this inside the applyBlur() function, above the save work request.
+        // Create charging constraint
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .build()
+
         // Add WorkRequest to save the image to the filesystem
-        val save = OneTimeWorkRequest.Builder(SaveImageToFileWorker::class.java).build()
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .setConstraints(constraints)
+            .addTag(TAG_OUTPUT)
+            .build()
         continuation = continuation.then(save)
 
         // Actually start the work
@@ -95,6 +109,10 @@ class BlurViewModel(application: Application) : ViewModel() {
             builder.putString(KEY_IMAGE_URI, imageUri.toString())
         }
         return builder.build()
+    }
+
+    internal fun cancelWork() {
+        workManager.cancelUniqueWork(IMAGE_MANIPULATION_WORK_NAME)
     }
 
     class BlurViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
