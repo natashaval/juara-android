9ada6695042ac765b57941feabc89eb23b13a802
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-10-17 09:25:48 +0700

    tip amount

--- app/src/main/java/com/example/happybirthday/compose/view/TipActivity.kt
+++ app/src/main/java/com/example/happybirthday/compose/view/TipActivity.kt
@@ -3,22 +3,24 @@ package com.example.happybirthday.compose.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.R
 import com.example.happybirthday.compose.view.ui.theme.HappyBirthdayTheme
+import java.text.NumberFormat
 
 class TipActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +47,39 @@ fun TipTimeScreen() {
       fontSize = 24.sp,
       modifier = Modifier.align(Alignment.CenterHorizontally)
     )
+    Spacer(modifier = Modifier.height(16.dp))
+    EditNumberField()
+    Spacer(Modifier.height(24.dp))
+    Text(
+      text = stringResource(id = R.string.tip_amount, ""),
+      modifier = Modifier.align(Alignment.CenterHorizontally),
+      fontSize = 20.sp,
+      fontWeight = FontWeight.Bold
+    )
   }
+}
+
+@Composable
+fun EditNumberField() {
+  var amountInput by remember { mutableStateOf("0") }
+  val amount = amountInput.toDoubleOrNull() ?: 0.0
+  val tip = calculateTip(amount)
+  TextField(
+    value = amountInput,
+    onValueChange = { amountInput = it },
+    label = { Text(text = stringResource(id = R.string.cost_of_service))},
+    modifier = Modifier.fillMaxWidth(),
+    singleLine = true,
+    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+  )
+}
+
+private fun calculateTip(
+  amount: Double,
+  tipPercent: Double = 15.0
+): String {
+  val tip = tipPercent / 100 * amount
+  return NumberFormat.getCurrencyInstance().format(tip)
 }
 
 @Preview(showBackground = true)
