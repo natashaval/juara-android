312c7089c2780e99b86c819b8190e93c292f2b68
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-11-04 23:45:23 +0700

    greetings card

--- app/src/main/java/com/example/happybirthday/compose/view/BasicCodelabActivity.kt
+++ app/src/main/java/com/example/happybirthday/compose/view/BasicCodelabActivity.kt
@@ -4,22 +4,26 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.Card
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.happybirthday.R
 import com.example.happybirthday.compose.view.ui.theme.BasicsCodelabTheme
 import com.example.happybirthday.compose.view.ui.theme.HappyBirthdayTheme
 
@@ -36,32 +40,51 @@ class BasicCodelabActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-  var expanded by remember { mutableStateOf(false) }
-
-  val extraPadding by animateDpAsState(
-    if (expanded) 48.dp else 0.dp,
-    animationSpec = spring(
-      dampingRatio = Spring.DampingRatioMediumBouncy,
-      stiffness = Spring.StiffnessLow
-    )
-  )
-  Surface(
-    color = MaterialTheme.colorScheme.primary,
+  Card(
+    backgroundColor = MaterialTheme.colorScheme.primary,
     modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
   ) {
-    Row(modifier = Modifier.padding(24.dp)) {
-      Column(modifier = Modifier
+    CardContent(name = name)
+  }
+}
+
+@Composable
+private fun CardContent(name: String) {
+  var expanded by remember { mutableStateOf(false) }
+  Row(
+    modifier = Modifier
+      .padding(12.dp)
+      .animateContentSize(
+        animationSpec = spring(
+          dampingRatio = Spring.DampingRatioMediumBouncy,
+          stiffness = Spring.StiffnessLow
+        )
+      )
+  ) {
+    Column(
+      modifier = Modifier
         .weight(1f)
-        .padding(bottom = extraPadding)
-      ) {
-        Text(text = "Hello, ")
-        Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+        .padding(12.dp)
+    ) {
+      Text(text = "Hello, ")
+      Text(
+        text = name, style = MaterialTheme.typography.headlineMedium.copy(
           fontWeight = FontWeight.ExtraBold
-        ))
+        )
+      )
+      if (expanded) {
+        Text(
+          text = ("Composem ipsum color sit lazy, " +
+              "padding theme elit, sed do bouncy. ").repeat(4),
+        )
       }
-      ElevatedButton(onClick = { expanded = !expanded }) {
-        Text(text = if (expanded) "Show less" else "Show more")
-      }
+    }
+    IconButton(onClick = { expanded = !expanded }) {
+      Icon(
+        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+        contentDescription = if (expanded) stringResource(id = R.string.show_less)
+        else stringResource(id = R.string.show_more)
+      )
     }
   }
 }
@@ -73,10 +96,10 @@ private fun MyApp(
 ) {
   var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
-  Surface(modifier) {
+  Surface(modifier, color = MaterialTheme.colorScheme.background) {
     if (shouldShowOnboarding) {
       OnboardingScreen(
-        onContinueClicked = { shouldShowOnboarding = false}
+        onContinueClicked = { shouldShowOnboarding = false }
       )
     } else {
       Greetings(Modifier, names)
