4c86b42ade313f8b9a2611a0055014fbd859c446
Natasha Santoso <natasha.valentina1998@gmail.com>
2022-11-10 23:36:27 +0700

    init sunflower migrate xml

---
+++ app/src/main/java/com/example/happybirthday/compose/sunflower/plantdetail/PlantDetailDescription.kt
@@ -0,0 +1,15 @@
+package com.example.happybirthday.compose.sunflower.plantdetail
+
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * Created by natasha.santoso on 10/11/22.
+ */
+@Composable
+fun PlantDetailDescription() {
+  Surface {
+    Text("Hello Compose")
+  }
+}
